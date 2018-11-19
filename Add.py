33bfcc0fc60ca8986df44d92777a1a15f0e41a42
Sergey Bokhnyak <sergeybok@gmail.com>
2018-11-19 18:12:46 +0100

    Addition wrapper

--- Add.py
+++ Add.py
@@ -1,14 +1,34 @@
 import numpy as np
 import itertools
 
-np.random.seed(0)
 
-def generate_add_data(n,max_t):
+
+
+class AddingDataWrapper(object):
+    def __init__(self,batch_size, min_T, max_T,seed=1):
+        self.min_T = self.clock = min_T
+        self.max_T = max_T
+        self.batch_size = batch_size
+        self.rng = np.random.RandomState(seed)
+
+    def next_batch(self):
+        X,Y,T = generate_add_data(self.batch_size,self.clock,self.rng)
+        self.clock += 1
+        if self.clock > self.max_T:
+            self.clock = self.min_T
+        return X,Y,T
+
+
+
+
+
+
+def generate_add_data(n,max_t,rng):
     #n: number of samples
     #max_t: maximum length of sequence
     max_l = max_t*2
-    noise = np.random.uniform(0,1,(n,max_l))
-    T = np.random.randint(1,max_t,(n))
+    noise = rng.uniform(0,1,(n,max_l))
+    T = rng.randint(1,max_t,(n))
     max_roll = max_l-T-1
 
     mask = np.zeros((n,max_l))
@@ -19,7 +39,7 @@ def generate_add_data(n,max_t):
 
     rolls = []
     for roll in max_roll:
-        rolls.append(np.random.randint(0,roll,1))
+        rolls.append(rng.randint(0,roll,1))
     rolls = np.vstack(rolls)
 
     for i,r in enumerate(rolls):
@@ -29,5 +49,4 @@ def generate_add_data(n,max_t):
     noise = noise.reshape(-1,max_l,1)
     x = np.concatenate([mask,noise],2)
     y = (mask*noise).sum(axis=1).reshape(-1,1)
-    print('{:2.2f}% of n are max T'.format((T==max_t-1).sum()/n*100))
     return x,y,T
