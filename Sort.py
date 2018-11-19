88c57d3256dc1fa6de706046b463d063fe508f72
Sergey Bokhnyak <sergeybok@gmail.com>
2018-11-19 17:40:54 +0100

    sorting experiment

--- Sort.py
+++ Sort.py
@@ -1,6 +1,39 @@
 import numpy as np 
 
+
+
+
 class SortingDataWrapper(object):
+    def __init__(self,batch_size,min_T,max_T,seed=1):
+        self.rng = np.random.RandomState(seed)
+        self.batch_size = batch_size
+        self.num_elements = num_elements
+        self.clock = self.min_T = min_T
+        self.max_T = max_T
+
+    def next_batch(self):
+        data = []
+        targets = []
+        for i in range(self.batch_size):
+            x,y = self.generate(self.num_elements,self.clock)
+            data.append(x)
+            targets.append(y)
+        self.clock += 1
+        if self.clock > self.max_T:
+            self.clock = self.min_T
+        return np.array(data), np.array(targets)
+
+    def generate(self,T):
+        X = np.zeros((T*2, 2))
+        X[T-1,0] = 1.0
+        x = self.rng.uniform(-1,1,(T))
+        X[:T,1] = x
+        y = np.argsort(x)
+        Y = np.zeros(T*2)
+        Y[T:] = y
+        return X, Y
+ 
+class __SortingDataWrapper(object):
     def __init__(self,batch_size,num_elements,min_T,max_T,seed=1):
         self.rng = np.random.RandomState(seed)
         self.batch_size = batch_size
@@ -22,11 +55,13 @@ class SortingDataWrapper(object):
 
     def generate(self,num_elem,T):
         idx = np.arange(T)
-        x = self.rng.uniform(0,1,(T,num_elem+1))
+        x = self.rng.uniform(0,1,(T,num_elem))
         self.rng.shuffle(idx)
         x[:,0] = idx 
         y = x[:,1:]
-        y = y[idx]
+        #y = y[idx]
+        tmp = x[:,1:]
+        x[:,1:] = tmp[:,idx]
         return x, y
     
 
