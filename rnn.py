1ef81e6151717af9ed2cc5d4906244f921d33964
Heng xin Fun <hengfun@gmail.com>
2018-11-22 13:26:31 +0100

    mnist early stopping working

--- rnn.py
+++ rnn.py
@@ -54,6 +54,22 @@ def lstm_ortho_initializer(scale=1.0):
 
   return _initializer
 
+def gru_ortho_initializer(scale=1.0):
+  """gru orthogonal initializer."""
+  def _initializer(shape, dtype=tf.float32,
+                   partition_info=None):  # pylint: disable=unused-argument
+    size_x = shape[0]
+    size_h = shape[1] // 4  # assumes lstm.
+    t = np.zeros(shape)
+    t[:, :size_h] = orthogonal([size_x, size_h]) * scale
+    t[:, size_h:size_h * 2] = orthogonal([size_x, size_h]) * scale
+    t[:, size_h * 2:size_h * 3] = orthogonal([size_x, size_h]) * scale
+    t[:, size_h * 3:] = orthogonal([size_x, size_h]) * scale
+    return tf.constant(t, dtype)
+
+  return _initializer
+
+
 
 class LSTMCell(tf.contrib.rnn.RNNCell):
   """Vanilla LSTM cell.
@@ -118,6 +134,90 @@ class LSTMCell(tf.contrib.rnn.RNNCell):
       new_h = tf.tanh(new_c) * tf.sigmoid(o)
 
       return new_h, tf.concat([new_c, new_h], 1)  # fuk tuples.
+
+
+
+
+class GRUCell(tf.contrib.rnn.RNNCell):
+  """Vanilla GRU cell.
+
+  Uses ortho initializer, and also recurrent dropout without memory loss
+  (https://arxiv.org/abs/1603.05118)
+  """
+
+  def __init__(self,
+               num_units,
+               use_recurrent_dropout=False,
+               dropout_keep_prob=0.9):
+    self.num_units = num_units
+    self.use_recurrent_dropout = use_recurrent_dropout
+    self.dropout_keep_prob = dropout_keep_prob
+
+  @property
+  def state_size(self):
+    return self.num_units
+
+  @property
+  def output_size(self):
+    return self.num_units
+
+  def get_output(self, h):
+    return h
+
+  def __call__(self, x, h, scope=None):
+    with tf.variable_scope(scope or type(self).__name__):
+      #c, h = tf.split(state, 2, 1)
+
+      x_size = x.get_shape().as_list()[1]
+
+      w_init = None  # uniform
+
+      #h_init = lstm_ortho_initializer(1.0)
+      h_init = tf.orthogonal_initializer(1.0)
+
+      # Keep W_xh and W_hh separate here as well to use different init methods.
+      w_xh = tf.get_variable(
+          'W_xh', [x_size, 2 * self.num_units], initializer=w_init)
+      w_hh = tf.get_variable(
+          'W_hh', [self.num_units, 2 * self.num_units], initializer=h_init)
+      bias = tf.get_variable(
+          'bias', [2 * self.num_units],
+          initializer=tf.constant_initializer(0.0))
+
+      w2_xh = tf.get_variable(
+        'W_xh2', [x_size, 1 * self.num_units], initializer=w_init)
+      w2_hh = tf.get_variable(
+        'W_hh2', [self.num_units, 1 * self.num_units], initializer=h_init)
+      bias2 = tf.get_variable(
+        'bias2', [1 * self.num_units],
+        initializer=tf.constant_initializer(0.0))
+
+      concat = tf.concat([x, h], 1)
+      w_full = tf.concat([w_xh, w_hh], 0)
+      hidden = tf.matmul(concat, w_full) + bias
+
+      z, r = tf.split(hidden, 2, 1)
+      # concat2 = tf.concat([])
+      h_candidate=tf.tanh(tf.matmul(x,w2_xh)+tf.matmul(tf.multiply(r,h),w2_hh)+bias2)
+
+
+      new_h = tf.multiply(z,h) + tf.multiply((1.0 - z),h_candidate)
+
+      #if self.use_recurrent_dropout:
+      #  g = tf.nn.dropout(tf.tanh(j), self.dropout_keep_prob)
+      #else:
+      #  g = tf.tanh(j)
+
+      #new_c = c * tf.sigmoid(f + self.forget_bias) + tf.sigmoid(i) * g
+      #new_h = tf.tanh(new_c) * tf.sigmoid(o)
+
+
+
+      return new_h, new_h
+
+
+
+
 
 
 def layer_norm_all(h,
--- run_mnist.py
+++ run_mnist.py
@@ -6,32 +6,28 @@ from Mnist import MnistWrapper
 from tensorflow.examples.tutorials.mnist import input_data
 
 
-n_steps = 10000
-
 
 logdir = './logs/mnist/{0}_{1}_{2}_clip{3}_hidden{4}'
 
 
 class Config(object):
     def __init__(self):
-        self.learning_rate = 10e-6
+        self.learning_rate = 1e-5
         self.seed = 1
+        self.seeds = 5
+        self.gpu = .12
+        self.n_steps  = 50000
         self.optimizer = 'adam'
         self.hidden_units = 250
         self.dtype = 'float32'
-        self.model = 'lstm'
-        self.validate_freq = 50
+        self.model = 'gru'
+        self.validate_freq = 10
         self.batch_size = 50
         self.clip_grad_norm = True
         if self.clip_grad_norm:
             self.max_norm_grad = 1.0
         else:
             self.max_norm_grad = None
-
-
-# gpu = args.gpu
-gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=.4)
-
 
 
 
@@ -51,10 +47,14 @@ class MNISTModel(object):
             self.dtype = tf.float32
         
         if args.model == 'lstm':
-            self.cell = LSTMCell(num_units=self.hidden_units)
+            # self.cell = LSTMCell(num_units=self.hidden_units)
+            self.cell = tf.contrib.cudnn_rnn.CudnnCompatibleLSTMCell(num_units=self.hidden_units)
             # self.cell = tf.contrib.cudnn_rnn.CudnnLSTM(num_layers=1,num_units=self.hidden_units)
         elif args.model == 'gru':
-            self.cell = tf.contrib.rnn.GRUCell(num_units=self.hidden_units)
+
+            self.cell = tf.contrib.cudnn_rnn.CudnnCompatibleGRUCell(num_units=self.hidden_units)
+            #self.cell = tf.nn.rnn_cell.GRUCell(num_units=self.hidden_units,kernel_initializer=tf.orthogonal_initializer())
+            # self.cell = GRUCell(num_units=self.hidden_units)
         
         self.X = tf.placeholder(dtype=self.dtype,shape=[None,28*28,1])
         self.batch_size = tf.shape(self.X)[0]
@@ -70,13 +70,17 @@ class MNISTModel(object):
 
         self.lr = tf.placeholder(dtype=self.dtype,name='learning_rate')
 
-        self.initial_state = self.cell.zero_state(batch_size=self.batch_size,dtype=self.dtype)
+        # self.initial_state = self.cell.zero_state(batch_size=self.batch_size,dtype=self.dtype)
 
 
         # self.X_ = tf.unstack(self.X,28*28,1)
         # output, last_state = tf.nn.static_rnn(self.cell,self.X_,dtype=self.dtype)
         # self.last_hidden = output[-1]
-        output, last_state = tf.nn.dynamic_rnn(self.cell,self.X,dtype=self.dtype)
+        if args.model == 'lstm':
+            output, last_state = tf.nn.dynamic_rnn(self.cell,self.X,dtype=self.dtype)
+        else:
+            output,last_state = tf.nn.dynamic_rnn(self.cell, self.X, dtype=self.dtype)
+
         # self.last_hidden = output[:,-1,:]
         self.last_hidden = tf.reshape(tf.slice(output,[0,28*28-1,0],[-1,-1,-1]),[self.batch_size,self.hidden_units])
 
@@ -114,51 +118,103 @@ print('building model..')
 
 
 args = Config()
-tf.set_random_seed(args.seed)
-model = MNISTModel(args)
+#tf.set_random_seed(args.seed)
+
 
 
 print('getting data..')
 
-data = MnistWrapper(batch_size=args.batch_size)
+data = MnistWrapper(batch_size=args.batch_size,seed=0)
 
 
 
 
-print('begin training for {0} steps'.format(n_steps))
-
-sess = tf.InteractiveSession(config=tf.ConfigProto(gpu_options=gpu_options))
-sess.run(tf.initializers.global_variables())
-
-writer = tf.summary.FileWriter(logdir.format(args.model,args.learning_rate,args.seed,args.max_norm_grad,args.hidden_units), sess.graph)
-train_error_summary = tf.summary.scalar(name='Train_Error',tensor=model.loss_batch)
-train_accuracy_summary = tf.summary.scalar(name='train_Accuracy',tensor=model.accuracy)
-valid_error_summary = tf.summary.scalar(name='Validation_Error',tensor=model.loss_batch)
-valid_accuracy_summary = tf.summary.scalar(name='Validation_Accuracy',tensor=model.accuracy)
-
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
-for step in range(n_steps):
-    tx,ty = data.next_batch()
-    #tx, ty = mnist.train.next_batch(args.batch_size)
-    _, e,a, summ,asumm = sess.run([model.train_step,model.loss_batch,model.accuracy,train_error_summary,train_accuracy_summary],{model.X:tx,model.Y:ty})
-    writer.add_summary(summ,step)
-    writer.add_summary(asumm,step)
-
-    if step % args.validate_freq == 0:
-
-        #vx, vy = mnist.test.next_batch(args.batch_size)
-        #_, ve, vacc, summ, asumm = sess.run(
-        #    [model.train_step, model.loss_batch, model.accuracy, valid_error_summary, valid_accuracy_summary],
-        #    {model.X: vx, model.Y: vy})
-
-        vx, vy = data.validate()
-        for i in range(30):
-            ve, vacc, esumm,asumm = sess.run([model.loss_batch,model.accuracy,valid_error_summary,valid_accuracy_summary],{model.X:vx[i*vx.shape[0]//30:(i+1)*vx.shape[0]//30],model.Y:vy[i*vx.shape[0]//30:(i+1)*vx.shape[0]//30]})
-            writer.add_summary(esumm,step)
-            writer.add_summary(asumm,step)
-        print('step {0} | e {1} a {2} | e {3} a {4}'.format(step,round(e,4),round(a,3),round(ve,4),round(vacc,3)))
 
 
+
+import datetime
+tstart = datetime.datetime.now()
+
+
+
+for seed in range(args.seeds):
+    # set seed
+    tf.reset_default_graph()
+    tf.set_random_seed(seed)
+    model = MNISTModel(args)
+
+    print('begin training for {0} steps'.format(args.n_steps))
+
+    sess = tf.InteractiveSession(config=tf.ConfigProto(gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=args.gpu)))
+    sess.run(tf.initializers.global_variables())
+
+    writer = tf.summary.FileWriter(
+        logdir.format(args.model, args.learning_rate, seed, args.max_norm_grad, args.hidden_units), sess.graph)
+    train_error_summary = tf.summary.scalar(name='Train_Error', tensor=model.loss_batch)
+    train_accuracy_summary = tf.summary.scalar(name='Train_Accuracy', tensor=model.accuracy)
+    valid_error_summary = tf.summary.scalar(name='Validation_Error', tensor=model.loss_batch)
+    valid_accuracy_summary = tf.summary.scalar(name='Validation_Accuracy', tensor=model.accuracy)
+
+    hist_loss = []
+    patience = 10
+    min_delta = 0.01
+    patience_cnt = 0
+
+    for step in range(args.n_steps):
+        tx,ty = data.next_batch()
+        #tx, ty = mnist.train.next_batch(args.batch_size)
+        _, e,a, summ,asumm = sess.run([model.train_step,model.loss_batch,model.accuracy,train_error_summary,train_accuracy_summary],{model.X:tx,model.Y:ty})
+        writer.add_summary(summ,step)
+        writer.add_summary(asumm,step)
+
+        if step % args.validate_freq == 0:
+
+            #vx, vy = mnist.test.next_batch(args.batch_size)
+            #_, ve, vacc, summ, asumm = sess.run(
+            #    [model.train_step, model.loss_batch, model.accuracy, valid_error_summary, valid_accuracy_summary],
+            #    {model.X: vx, model.Y: vy})
+
+            vx, vy = data.validate()
+            valid_errors = []
+            for i in range(60):
+                ve, vacc, esumm,asumm = sess.run([model.loss_batch,model.accuracy,valid_error_summary,valid_accuracy_summary],{model.X:vx[i*vx.shape[0]//60:(i+1)*vx.shape[0]//60],model.Y:vy[i*vx.shape[0]//60:(i+1)*vx.shape[0]//60]})
+                writer.add_summary(esumm,step)
+                writer.add_summary(asumm,step)
+                valid_errors.append(ve.mean())
+            VE = sum(valid_errors) / float(len(valid_errors))
+            hist_loss.append(VE)
+            print('step {0} | e {1} a {2} | e {3} a {4}'.format(step,round(e,4),round(a,3),round(VE,4),round(vacc,3)))
+            if step>1000:
+                if hist_loss[-2] - hist_loss[-1] > min_delta:
+                    patience_cnt = 0
+                else:
+                    patience_cnt += 1
+                if patience_cnt > patience:
+                    print('early stopping')
+
+                    # call some function to test on test set
+                    break
+
+    vx, vy = data.test()
+    valid_errors = []
+    accs = []
+    for i in range(60):
+        te, tacc = sess.run([model.loss_batch, model.accuracy],
+                                          {model.X: vx[i * vx.shape[0] // 60:(i + 1) * vx.shape[0] // 60],
+                                           model.Y: vy[i * vx.shape[0] // 60:(i + 1) * vx.shape[0] // 60]})
+        valid_errors.append(ve.mean())
+
+        accs.append((tacc.mean()))
+    TestErr = sum(valid_errors) / float(len(valid_errors))
+    TestAcc = sum(accs) / float(len(accs))
+    dir_path = logdir.format(args.model, args.learning_rate, seed, args.max_norm_grad, args.hidden_units)
+    with open(dir_path+"/final_test_error.txt",'w') as f:
+        out = "Test Error:{} Test Accuracy:{}".format(TestErr,TestAcc)
+        f.write(out)
+    print(out)
+    tend = datetime.datetime.now()
+    print('ran {0} steps in {1} seconds'.format(args.n_steps,tend - tstart))
+    sess.close()
 
 
 
