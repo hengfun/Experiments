c8b85db35fad890d39f519f63419034749478ab3
hengfun <hengfun@gmail.com>
2018-11-22 10:44:56 +0100

    Fixed to use our mnist loader

--- run_mnist.py
+++ run_mnist.py
@@ -56,15 +56,15 @@ class MNISTModel(object):
         elif args.model == 'gru':
             self.cell = tf.contrib.rnn.GRUCell(num_units=self.hidden_units)
         
-        self.X = tf.placeholder(dtype=self.dtype,shape=[None,28*28])
+        self.X = tf.placeholder(dtype=self.dtype,shape=[None,28*28,1])
         self.batch_size = tf.shape(self.X)[0]
         self.mask = self.x_flat = tf.reshape(self.X,[-1])
-        self.X_ = tf.reshape(self.X, [self.batch_size,28*28,1])
+        #self.X_ = tf.reshape(self.X, [self.batch_size,28*28,1])
 
-        #self.Y = tf.placeholder(dtype=tf.int32,shape=[None])
-        #self.Y_onehot = tf.one_hot(self.Y,self.num_classes)
-        self.Y = tf.placeholder(dtype=tf.int32,shape=[None,10])
-        self.Y_onehot = self.Y
+        self.Y = tf.placeholder(dtype=tf.int32,shape=[None])
+        self.Y_onehot = tf.one_hot(self.Y,self.num_classes)
+        #self.Y = tf.placeholder(dtype=tf.int32,shape=[None,10])
+        #self.Y_onehot = self.Y
 
         self.sequence_length = tf.placeholder(dtype=self.dtype,name='sequence_lengths')
 
@@ -76,7 +76,7 @@ class MNISTModel(object):
         # self.X_ = tf.unstack(self.X,28*28,1)
         # output, last_state = tf.nn.static_rnn(self.cell,self.X_,dtype=self.dtype)
         # self.last_hidden = output[-1]
-        output, last_state = tf.nn.dynamic_rnn(self.cell,self.X_,dtype=self.dtype)
+        output, last_state = tf.nn.dynamic_rnn(self.cell,self.X,dtype=self.dtype)
         # self.last_hidden = output[:,-1,:]
         self.last_hidden = tf.reshape(tf.slice(output,[0,28*28-1,0],[-1,-1,-1]),[self.batch_size,self.hidden_units])
 
@@ -87,7 +87,7 @@ class MNISTModel(object):
         #self.accuracy = tf.reduce_sum(tf.cast(tf.equal(self.Y,self.prediction),tf.float32)) / self.batch_size
         # self.accuracy = tf.reduce_sum(tf.cast(tf.equal(self.Y,self.prediction),tf.int32)) / self.batch_size
 
-        correct_pred = tf.equal(self.prediction, tf.cast(tf.argmax(self.Y, 1),tf.int32))
+        correct_pred = tf.equal(self.prediction, self.Y)
         self.accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
 
@@ -138,24 +138,24 @@ valid_accuracy_summary = tf.summary.scalar(name='Validation_Accuracy',tensor=mod
 
 mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
 for step in range(n_steps):
-    # tx,ty = data.next_batch()
-    tx, ty = mnist.train.next_batch(args.batch_size)
+    tx,ty = data.next_batch()
+    #tx, ty = mnist.train.next_batch(args.batch_size)
     _, e,a, summ,asumm = sess.run([model.train_step,model.loss_batch,model.accuracy,train_error_summary,train_accuracy_summary],{model.X:tx,model.Y:ty})
     writer.add_summary(summ,step)
     writer.add_summary(asumm,step)
 
     if step % args.validate_freq == 0:
 
-        vx, vy = mnist.test.next_batch(args.batch_size)
-        _, ve, vacc, summ, asumm = sess.run(
-            [model.train_step, model.loss_batch, model.accuracy, valid_error_summary, valid_accuracy_summary],
-            {model.X: vx, model.Y: vy})
+        #vx, vy = mnist.test.next_batch(args.batch_size)
+        #_, ve, vacc, summ, asumm = sess.run(
+        #    [model.train_step, model.loss_batch, model.accuracy, valid_error_summary, valid_accuracy_summary],
+        #    {model.X: vx, model.Y: vy})
 
-        #vx, vy = data.validate()
-        #for i in range(30):
-        #    ve, vacc, esumm,asumm = sess.run([model.loss_batch,model.accuracy,valid_error_summary,valid_accuracy_summary],{model.X:vx[i*vx.shape[0]//30:(i+1)*vx.shape[0]//30],model.Y:vy[i*vx.shape[0]//30:(i+1)*vx.shape[0]//30]})
-        #    writer.add_summary(esumm,step)
-        #    writer.add_summary(asumm,step)
+        vx, vy = data.validate()
+        for i in range(30):
+            ve, vacc, esumm,asumm = sess.run([model.loss_batch,model.accuracy,valid_error_summary,valid_accuracy_summary],{model.X:vx[i*vx.shape[0]//30:(i+1)*vx.shape[0]//30],model.Y:vy[i*vx.shape[0]//30:(i+1)*vx.shape[0]//30]})
+            writer.add_summary(esumm,step)
+            writer.add_summary(asumm,step)
         print('step {0} | e {1} a {2} | e {3} a {4}'.format(step,round(e,4),round(a,3),round(ve,4),round(vacc,3)))
 
 
